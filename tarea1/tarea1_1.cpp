ea49e442245dfce1747c9d7628517f0e6fa43c08
Alejandro <alejandro.vergara.araneda@gmail.com>
2023-03-31 14:40:36 -0300

    creacion Tarea1

---
+++ tarea1/tarea1_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int const SERV_DESAYUNO = 0;
+int const SERV_ALMUERZO = 1;
+int const SERV_ONCE = 2;
+int const SERV_CENA = 3;
+
+int const R = 10;
+
+
+
+
+
+
+
+
+bool puedeConsumir(char* rut, int servicio, string consumos_dia){
+
+}
+
+
+
+
+int main(){
+/*
+    char rut[R],n;
+    for (int i=0 ; i<R ; i++){
+        cin>>n;
+        rut[i]=n;
+    }
+    cout<<"\n";
+    for (int i=0 ; i<R ; i++){
+        cout<<rut[i];
+    }
+    cout<<"\n";
+*/
+    return 0;
+}
