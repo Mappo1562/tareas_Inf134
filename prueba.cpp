13ba988f334744cbd660b0c99fd52b1886cc31ba
Alejandro <alejandro.vergara.araneda@gmail.com>
2023-04-05 18:49:19 -0400

    creacion funcion es_colaborador y comienzo

---
+++ prueba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int const R = 11;
+
+
+
+int main(){
+    cout<<"todo funciona \n";
+    return 0;
+}
--- tarea1/p1/tarea1_1.cpp
+++ tarea1/p1/tarea1_1.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 using namespace std;
+
+
 
 int const SERV_DESAYUNO = 0;
 int const SERV_ALMUERZO = 1;
 int const SERV_ONCE = 2;
 int const SERV_CENA = 3;
 
-int const R = 10;
+int const R = 11;
 
 struct SaldoColaborador {
 char rut [11];
@@ -17,39 +20,53 @@ int saldo_once ;
 int saldo_cena ;
 };
 
-/*
-void print(char *rut){
-    for (int i=0 ; i<R ; i++){
-        cout<<rut[i];
+
+
+bool es_colaborador(char*rut){
+    int n;
+    bool flag=0;
+    ifstream file;
+    SaldoColaborador cliente;
+    file.open("saldos.bin",ios::binary);
+    if(!file.is_open()){
+        cout << "Error al abrir el archivo" << endl;
+        exit(1);
     }
-    cout<<"\n";
+    file.read((char*)&n, sizeof(int));
+    cout<<"n = "<<n<<" \n";
+    for(int i=0 ; i<n ; i++){
+        file.read((char*)&cliente, sizeof(SaldoColaborador));
+        if (!strcmp(cliente.rut,rut))
+            flag=1;
+    }
+    file.close();
+    if (flag)
+    return true;
+    return false;
 }
-*/
 
 
 
 bool puedeConsumir(char* rut, int servicio, string consumos_dia){
-
+    if (es_colaborador(rut))
+        cout<<"es colaborador\n";
+    else 
+        cout<<"no es colaborador\n";
+    return false;
 }
 
 
 
-
 int main(){
-
-    char rut[R],n;
-    for (int i=0 ; i<R ; i++){
-        cin>>n;
-        rut[i]=n;
-    }
-    cout<<"\n";
-    print(rut);
+    char r[R]="33333333-3";
+    puedeConsumir(r,1,"consumosdia.txt");
     return 0;
 }
 
 
 
+
+
 /*
-43
-56
+http://jbgarcia.webs.uvigo.es/asignaturas/TO/cursilloCpp/14_archivos.html
 */
