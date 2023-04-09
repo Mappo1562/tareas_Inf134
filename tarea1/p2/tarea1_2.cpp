615db42402138fcec18d712734e2a33d3a675794
Alejandro <alejandro.vergara.araneda@gmail.com>
2023-04-09 18:58:01 -0400

    adelantao un poco

---
+++ tarea1/p2/tarea1_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+struct Persona {
+    string nombre ;
+    char fecha [11];
+    int tamanio_tarjeta ;
+    int * tarjeta ;
+    bool quiere_intercambiar ;
+};
+
+int* comprarTarjeta(string nombre, int dia, int &m){
+
+}
+
+void intercambiarTarjeta(Persona* p1, Persona* p2){
+
+}
+
+int puntaje(Persona* p1){
+
+}
+
+Persona* unDia(Persona* personas, int dia){
+
+}
+
+void variosDias(Persona* personas, int cant_dias){
+
+}
+
+int main(){
+    
+}
