ba4000a201eca613a655439f12cd01e0fd4b938b
Alejandro <alejandro.vergara.araneda@gmail.com>
2023-04-12 19:09:04 -0400

    a

--- tarea1/p2/tarea1_2.cpp
+++ tarea1/p2/tarea1_2.cpp
@@ -12,7 +12,11 @@ struct Persona {
 };
 
 int* comprarTarjeta(string nombre, int dia, int &m){
-
+    int ret[m];
+    for (int i=0 ; i<m ; i++){
+        ret[i]=nombre[i]%dia;
+    }
+    return ret;
 }
 
 void intercambiarTarjeta(Persona* p1, Persona* p2){
@@ -32,5 +36,22 @@ void variosDias(Persona* personas, int cant_dias){
 }
 
 int main(){
-    
+    ifstream file;
+    int n,tamanio;
+    string nombre,fecha;
+    bool cambia;
+    file.open("ejemplos.txt");
+    if(!file.is_open()){
+        cout << "Error al abrir el archivo" << endl;
+        exit(1);
+    }
+    file>>n;
+    for (int i=0;i<n;i++){
+        file>>nombre;
+        file>>fecha;
+        file>>cambia;
+        file>>tamanio;
+        cout<<comprarTarjeta(nombre,dia,tamanio)
+    }
+    return 0;
 }
